22eb97c60dd8a1f66e9a5e962ff221d99bc97dd1
Melina <melina.m.bustos@gmail.com>
2023-04-03 12:35:21 -0300

    3-4-23

--- Entregafinalmelinabustos/src/firebase/upload.js
+++
@@ -1,8 +0,0 @@
-import { db } from './config.js'
-import { addDoc, collection } from 'firebase/firestore'
-
-const productosRef = collection(db, 'productos')
-
-data.forEach((item) => {
-    addDoc(productosRef, item)
-})
